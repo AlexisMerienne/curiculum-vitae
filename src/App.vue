97d355f1c5da3d77f9897758aede6eeecfb97fde
AlexisMerienne <alexis.merienne@gmail.com>
2022-02-01 10:34:47 +0100

    home page - links to differents social networks

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <div class="header">
+        <div class="container-title" >
+          <div class="before-img" style="height:60px;width:20px"/>
+          <!--
+          <div class="navbar-logo" v-on:click="goToHomePage()">
+              <img src="../assets/logo/logo-alter-maison.png"/>
+          </div>
+          -->
+        </div>
+        <div class="before-img" style="height:100%;width:20px"/>
+        <b-button id='button-homepage' variant="outline-dark" v-on:click='goToHomePage()'>
+          Expérience professionnelles
+        </b-button>
+          <div class="before-img" style="height:100%;width:10px"/>
+          <b-button id="button-consommation" variant="outline-dark" v-on:click='goToTerritoireConso()'>
+           Études
+        </b-button>
+        <div class="before-img" style="height:100%;width:10px"/>
+        <b-button id="button-batiment" variant="outline-dark" v-on:click='goToBatiment()'>
+            Compétences principales
+        </b-button>
+    </div>
+    <CuriculumVue msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import CuriculumVue from "./components/Curiculum.vue"
+
+export default {
+  name: 'App',
+  components: {
+    CuriculumVue
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+  .header {
+    background-color: #1c8c00;
+    font-family:  'DM Sans', sans-serif;
+    font-size: 20px;
+    overflow: hidden;
+    display : flex;
+    align-items: center;
+    min-height : 60px;
+    width : 100%;
+    position : fixed;
+    z-index : 99;
+  }
+</style>
---
+++ src/components/Curiculum.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="main">
+    <div class="cv-container" shadow="">
+      <div class="nom-et-photo">
+        <div class="nom-et-description">
+          <h3>Alexis Mérienne<br></h3>
+          <h5>Etudiant en Master 1 d'informatique</h5>
+        </div>
+        <img class="my-profile-img" src="../assets/photo-profil-cv.jpg" width="130" height="144">
+      </div>
+      <div class="activity" style="margin-top : 20px">
+        
+      </div>
+    </div>
+    <div class="my-button" shadow-little="">
+      <a href="https://www.linkedin.com/in/alexis-m%C3%A9rienne-442948199/" style="color: #1c8c00;text-decoration: none">
+        <h5>Linkedin</h5>
+      </a>
+    </div>
+    <div class="my-button" shadow-little="">
+      <a href="https://github.com/AlexisMerienne" style="color: #1c8c00;text-decoration: none">
+        <h5>Github</h5>
+      </a>
+    </div>
+     <div class="my-button" shadow-little="">
+      <a href="https://www.facebook.com/alexis.Req1" style="color: #1c8c00;text-decoration: none">
+        <h5>Facebook</h5>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Curiculum',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  width: 100%;
+  height: 100%;
+}
+h5 {
+  margin-bottom: 0px;
+}
+.main{
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+.cv-container{
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  background-color:white;
+  margin-top : 70px ;
+  width: 35%;
+  min-width: 250px;
+  text-align: left;
+}
+.nom-et-photo{
+  display: flex;
+  flex-direction: row;
+  align-items: left;
+  justify-content: space-between;
+}
+.nom-et-description{
+  display: flex;
+  flex-direction: column;
+}
+.my-profile-img{
+  border-radius: 7px;
+}
+.my-button{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  margin-top: 30px;
+  width: 35%;
+  min-width: 250px;
+  min-height: 40px;
+  border:solid;
+  border-color: #7aeb7a;
+  border-radius : 14px;
+  cursor: pointer;
+}
+.my-button:hover{
+  background-color : #bfebbf;
+}
+</style>

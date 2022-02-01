311d7dc2133802ccf0768be6fa69424b1e86fd96
AlexisMerienne <alexis.merienne@gmail.com>
2022-02-01 14:37:37 +0100

    css debug

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div class="app" id="app">
     <div class="header">
         <div class="container-title" >
           <div class="before-img" style="height:60px;width:20px"/>
@@ -44,7 +44,7 @@ export default {
 </script>
 
 <style>
-  #app {
+  .app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;

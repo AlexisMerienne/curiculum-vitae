c59c8772662db8212500d50b4006348b4ff0f459
AlexisMerienne <alexis.merienne@gmail.com>
2022-02-04 17:36:35 +0100

    deploy

--- src/components/pages/Competences.vue
+++ src/components/pages/Competences.vue
@@ -19,6 +19,33 @@
                 </div>
             </div>
         </div>
+        <div id='flip-card-2' class="flip-card">
+            <div id='flip-card-container-2' class="flip-card-container">
+                <div class="front-card-container" v-on:click="rotate2()">
+                    <h5>Mes compétences pratiques</h5>
+                </div>
+                <div id="back-container" class="back-container" v-on:click="rotateback2()">
+                    <h6>&#x25CF;<strong>Science des données</strong><ul/></h6>
+                    <p>introduction à l’IA, Machine Learning, valorisation des données.<br></p>
+                    <h6>&#x25CF;<strong>Software</strong><ul/></h6>
+                    <p>architecture logicielle, Conception logicielle, IHM</p>
+                </div>
+            </div>
+        </div>
+        <div id='flip-card-3' class="flip-card">
+            <div id='flip-card-container-3' class="flip-card-container">
+                <div class="front-card-container" v-on:click="rotate3()">
+                    <h5>Langues</h5>
+                </div>
+                <div id="back-container" class="back-container" v-on:click="rotateback3()">
+                    <p>
+                        - Anglais - Niveau C1 (TOEIC : 955/990)<br>
+                        - Allemand - Niveau B1<br>
+                        - Italien - Niveau A2<br>
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -30,14 +57,25 @@ export default {
   },
   methods : {
     rotate() {
-    
-        //document.getElementById('flip-card').style.transform = "rotateY(180deg)";
-        document.getElementById('flip-card-container').style.transform = "rotateY(180deg)";
-        
+        document.getElementById('flip-card-container').style.transform = "rotateY(180deg)";     
     },
     rotateback(){
         document.getElementById('flip-card').style.transform = "rotateY(0deg)";
         document.getElementById('flip-card-container').style.transform = "rotateY(0deg)";
+    },
+    rotate2() {
+        document.getElementById('flip-card-container-2').style.transform = "rotateY(180deg)";     
+    },
+    rotateback2(){
+        document.getElementById('flip-card-2').style.transform = "rotateY(0deg)";
+        document.getElementById('flip-card-container-2').style.transform = "rotateY(0deg)";
+    },
+    rotate3() {
+        document.getElementById('flip-card-container-3').style.transform = "rotateY(180deg)";     
+    },
+    rotateback3(){
+        document.getElementById('flip-card-3').style.transform = "rotateY(0deg)";
+        document.getElementById('flip-card-container-3').style.transform = "rotateY(0deg)";
     },
   }
 }
@@ -77,6 +115,57 @@ p {
 }
 /* Do an horizontal flip when you move the mouse over the flip box container */
 .flip-card:hover {
+  cursor: pointer;
+}
+
+.flip-card-2{
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 30%;
+  min-width: 250px;
+  height : 300px;
+  margin: 10px;
+  margin-top : 80px ;
+  background-color: transparent;
+  border-radius: 7px;
+  perspective: 1000px; /* Remove this if you don't want the 3D effect */
+}
+.flip-card-container-2{
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+/* Do an horizontal flip when you move the mouse over the flip box container */
+.flip-card-2:hover {
+  cursor: pointer;
+}
+.flip-card-3{
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 30%;
+  min-width: 250px;
+  height : 300px;
+  margin: 10px;
+  margin-top : 80px ;
+  background-color: transparent;
+  border-radius: 7px;
+  perspective: 1000px; /* Remove this if you don't want the 3D effect */
+}
+.flip-card-container-3{
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+/* Do an horizontal flip when you move the mouse over the flip box container */
+.flip-card-3:hover {
   cursor: pointer;
 }
 
